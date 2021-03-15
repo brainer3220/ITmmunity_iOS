955e870788e59f0e89be1aaaf3444ae5f0855bbc
brainer <brainer@kakao.com>
2021-03-16 04:05:41 +0900

    data file move

---
+++ ITmmunity/DataStruct.swift
@@ -0,0 +1,41 @@
+//
+//  data.swift
+//  ITmmunity
+//
+//  Created by 진상현 on 2021/03/15.
+//
+
+import Foundation
+
+public var UrlList: Array<String> = [
+    "http://underkg.co.kr",
+    "https://meeco.kr",
+    "https://quasarzone.com",
+    "https://coolenjoy.net",
+]
+
+public struct Content: Hashable, Codable {
+    var title: String
+    var image: String
+    var hit: Int?
+    var numComment: Int
+    var url: String
+}
+
+public var dummies: Array<Content> =
+    [
+        Content(
+        title: "삼성전자, 갤럭시 엑스커버 프로 OneUI 3.0 업데이트 배포",
+        image: "www.underkg.co.kr",
+        hit: 784,
+        numComment: 0,
+        url: "www.underkg.co.kr"
+    ),
+        Content(
+        title: "삼성, 'Neo QLED', TV 업계 최초 독일 VDE '아이케어' 인증 획득)",
+        image: "www.underkg.co.kr",
+        hit: 404,
+        numComment: 10,
+        url: "www.underkg.co.kr"
+    )
+]
