dbee0c51c8cb21cadc3d6e67f7030f67e171a92f
brainer <brainer@kakao.com>
2021-03-16 20:27:22 +0900

    add Firebase function in App

--- ITmmunity/ITmmunityApp.swift
+++ ITmmunity/ITmmunityApp.swift
@@ -6,10 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ITmmunityApp: App {
     let persistenceController = PersistenceController.shared
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             MainList()
